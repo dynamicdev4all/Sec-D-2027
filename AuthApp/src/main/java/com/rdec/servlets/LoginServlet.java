908def6125a10bf4cfe2dc837b59cc0a30db808b
dynamicdev4all <149931467+dynamicdev4all@users.noreply.github.com>
2025-10-09 15:11:36 +0530

    Add files via upload

---
+++ AuthApp/src/main/java/com/rdec/servlets/LoginServlet.java
@@ -0,0 +1,56 @@
+package com.rdec.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email_key");
+		String password = request.getParameter("password_key");
+		System.out.println("Your email Address is : " + email);
+		System.out.println("Your Password is :" + password);
+		
+		//this is just for learning purpose, this code will be updated as soon as we are done with database implement
+		
+		if(email.equals("admin@rdec.in")&& password.equals("123456")) {
+//			response.sendRedirect("home_page.html");
+			HttpSession session = request.getSession();
+			session.setAttribute("userName", "Deepak");
+			response.sendRedirect("home.jsp");
+		}else {
+			System.out.println("Email or password is invalid");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
